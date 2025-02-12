b0dc14af924edfe55d5d22711201b3d04ce06906
Yasin Kereci <yke144907@stud.gibb.ch>
2025-02-12 22:56:49 +0100

    alert

---
+++ js-vs-vue/aap.js
@@ -0,0 +1,7 @@
+const todoText = document.getElementById('#todoText');
+const addBtn = document.getElementById('#addBtn');
+const todoList = document.getElementById('#todoList');
+
+addBtn.addEventListener('click', () => {
+    alert();
+}); 
