a78a2b27cfff2ef7857af3f5df93da5b65cd895f
Yasin Kereci <yke144907@stud.gibb.ch>
2025-02-13 00:25:23 +0100

    v-bind

--- 1-vue-instance/app.js
+++ 1-vue-instance/app.js
@@ -3,7 +3,11 @@ const app = Vue.createApp({
         return {
             title: 'The Final Empire', 
             content: 'The Final Empire is a fantasy novel written by American author Brandon Sanderson. It was published on July 17, 2006 by Tor Books and is the first novel in the Mistborn trilogy. The book is set in a dark fantasy world on a planet called Scadrial, where the sun is red, plants are brown, and the ground is constantly being covered under black volcanic ashfalls. The story follows Vin, a street urchin who discovers her magical abilities and joins a group of',
-            url : "https://www.youtube.com/?hl=TR",
+            youtube :{
+                url : "https://www.youtube.com/?hl=TR",
+                target: "_blank",
+                context: "Please, for Youtube",
+            },
         };
     },
  }).mount('#app');  // mount the app to the div with id app   
